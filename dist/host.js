1f1b0c2c5edc101bed1f402bc6db364d9e255e72
nedol <nedol@narod.ru>
2018-10-30 17:14:22 +0300

    30.10.18

--- dist/host.js
+++ dist/host.js
@@ -1,7 +1,2 @@
-let http = 'http://';
-/*
-let host_port = 'localhost:8081';////'nedol.ru/reserve/server/';//
-let host = '127.0.0.1';//"http://nedol.ru/reserve/server";//
- */
-let host_port = 'nedol.ru/server/';//
-let host = "https://nedol.ru/server";//
+let host_port = 'http://localhost:3000';//'https://nedol.ru/server';//
+
