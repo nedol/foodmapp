58968f3f45bd06a852bc7a626bc0acc539d7266a
kolobot <nedol@narod.ru>
2022-10-10 23:52:13 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,39 +10,32 @@ const NODE_ENV = process.env.NODE_ENV //|| 'development';
 
 module.exports = {
 
-    devtool: NODE_ENV ==='development'?'cheap-inline-module-source-map':false,
-    // context: __dirname+'/js',
+    devtool: NODE_ENV ==='development'?'cheap-inline-module-source-map':false
 
     entry: {
-        // 'supplier': __dirname+'/src/supplier/supplier.entry.js'
-        // ,'supplier_offer': __dirname+'/src/supplier/supplier.offer.frame.js'
-        // ,'supplier_profile':__dirname+'/src/profile/profile.supplier.js'
-        // ,'supplier_settings':__dirname+'/src/supplier/supplier.settings.js'
-        // ,'supplier_customer_frame': __dirname+'/src/supplier/customer.frame.js'
+         'supplier': __dirname+'/src/supplier/supplier.entry.js'
+         ,'supplier_offer': __dirname+'/src/supplier/supplier.offer.frame.js'
+         ,'supplier_profile':__dirname+'/src/profile/profile.supplier.js'
+         ,'supplier_settings':__dirname+'/src/supplier/supplier.settings.js'
+         ,'supplier_customer_frame': __dirname+'/src/supplier/customer.frame.js'
         // ,
-        // 'deliver': __dirname+'/src/deliver/deliver.entry.js'
-        // ,'deliver_offer': __dirname+'/src/deliver/deliver.offer.frame.js'
-        // ,'deliver_settings':__dirname+'/src/deliver/deliver.settings.js'
-        // ,'deliver_profile':__dirname+'/src/profile/profile.deliver.js'
-        // ,'deliver_customer_frame': __dirname+'/src/deliver/customer.frame.js'
+         'deliver': __dirname+'/src/deliver/deliver.entry.js'
+         ,'deliver_offer': __dirname+'/src/deliver/deliver.offer.frame.js'
+         ,'deliver_settings':__dirname+'/src/deliver/deliver.settings.js'
+         ,'deliver_profile':__dirname+'/src/profile/profile.deliver.js'
+         ,'deliver_customer_frame': __dirname+'/src/deliver/customer.frame.js'
         //
         'customer': __dirname+'/src/customer/customer.entry.js'
         ,'customer_order': __dirname+'/src/customer/customer.order.frame.js'
         ,'customer_profile':__dirname+'/src/profile/profile.customer.js'
         ,'customer_settings':__dirname+'/src/customer/customer.settings.js'
-
-        // ,'foodtruck_order':__dirname+'/src/foodtruck/foodtruck.order.js'
-        // ,'customer_search':__dirname+'/src/search/customer.search.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
         library: '[name]'
     },
-    // watch: NODE_ENV ==='development',
-    // watchOptions: {
-    //     aggregateTimeout:100
-    // },
+
     module: {
     noParse:/webpack-jquery-ui\/tabs.js/,
     noParse:/jquery-ui\/dialog.js/,
@@ -103,20 +96,11 @@ rules: [
         new webpack.DefinePlugin({
             'process.browser': 'true'
         }),
-        //new webpack.IgnorePlugin(/your_package_name_here/),
-        // new HtmlWebpackPlugin({
-        //    template: './dist/main.tmplt.html'
-        // }),
 
         new webpack.NoEmitOnErrorsPlugin(),
         new ExtractTextPlugin('./src/html/css/main.css'),
 
-        //, new webpack.optimize.CommonsChunkPlugin({
-        //     name:'common'
-        // })
-        // new JavaScriptObfuscator({
-        //     rotateUnicodeArray: true
-        // }, ['supplier.js,customer.js'])
+
     ],
     resolve: {
         alias: {
